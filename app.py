b994700ed81d90a6cb630195eeb9be95111a77dc
Arnold Schultz <dramicos@yahoo.com>
2022-10-14 20:52:05 -0500

    updated everything working slider now

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from flask import Flask, render_template, jsonify
+from flask_pymongo import PyMongo
 import json
+import ev_ranges
 
 # Create an instance of Flask
 app = Flask(__name__)
 
+# Use PyMongo to establish Mongo connection
+conn = "mongodb://localhost:27017/ev_data"
+mongo = PyMongo(app, uri = conn)
+
+# Call function to populate ranges collection in mongo
+ev_range = ev_ranges.ranges()
+mongo.db.ranges.update_one({}, {"$set": ev_range}, upsert=True)
+
 # Route to render index.html
 @app.route("/")
 def home():
-
+    
     # Return template and data
     return render_template("index.html")
 
@@ -19,19 +29,24 @@ def map():
     return render_template("map.html")
 
 
-# Route that will stor the range json
-@app.route("/data/<filename>")
-def data(filename):
+# # Route that will store the range json
+@app.route("/ev-ranges")
+def ReadMongoRanges():
     
-    filepath = f"Data/{filename}"
-    try:
-        with open(filepath) as f:
-            json_data = json.load(f)
-    except:
-        json_data = {'Error' : f'{filename} not found on server'}
+    result = mongo.db.ranges.find()
+    result_list = list(result)
+    
+    if len(result_list) > 0:
+        # Return the first result only and strip off the '_id'
+        data = result_list[0] 
+        id_to_discard = data.pop('_id', None)
+    else:
+        # Construct an error message
+        data = {'Error': 'No data found'}        
 
+    print('Returning data from MongoDB')
 
-    return jsonify(json_data)
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,8 @@
     
     console.log("hello");
 
-    const rangeData = "data/EV-Ranges.json";
+    const rangeData = "ev-ranges";
+
 
     function init() {
         
@@ -13,13 +14,79 @@
     
     // Call the initialization
     init();
-     
+    
+    // Creates call to slider
+    $("#years").slider({});
+    
+    // Create action event listener
+    $("#years").on('slide', function (range) {
+    let timeString = $('#years').val();
+    let timeArray = timeString.split(',');
+    let yMin = timeArray[0];
+    let yMax = timeArray[1];
+    rangePlot(yMin,yMax);
+    });
+
     // Use plotly maps with JSON data to make a horizontal bar graph of top 10 cultures
-    function rangePlot(yearMin = 2002, yearMax = 2021) {
+    function rangePlot(yearMin = 2010, yearMax = 2021) {
         d3.json(rangeData).then(function(data) {
             // Setting the initial plot parameters
             console.log("rangePlot: ", data);
-            
+
+            // some error proofing -- Add more later
+            if (yearMin < 2010){
+                yearMin = 2010
+            }
+
+
+
+            // Setting the initial plot parameters
+            let ranges = data.data
+
+            // Create indexes for slicing from the years
+            let minYearIndex = yearMin - 2010;
+            let maxYearIndex = yearMax - 2010;
+       
+            let plotSample = ranges.slice(minYearIndex,maxYearIndex+1);
+            console.log("yearsPlot: ", plotSample);
+
+            // splitting the data into separate plotting variables
+            let  plotYears = [];
+            let  plotAvgRange = [];
+            let  plotMaxRange = [];
+            for (let i =0; i < plotSample.length; i++){
+                plotYears.push(plotSample[i][0]);
+                plotAvgRange.push(plotSample[i][1]);
+                plotMaxRange.push(plotSample[i][2]);
+            }
+
+            let plot1 = {
+            type : 'bar',
+            name : 'Average Ranges',
+            x: plotYears,
+            y: plotAvgRange,     
+            };
+
+            let plot2 = {
+                type : 'bar',
+                name : 'Max Ranges',
+                x: plotYears,
+                y: plotMaxRange,     
+            };
+
+            let trace = [plot1, plot2];
+
+            let layout = {
+                title : "Average and Maximum Ranges for EVs over the years",
+                margin: {t: 50, l: 140},
+                barmode: 'group'
+            };
+
+            let config = {responsive: true};
+      
+            Plotly.newPlot("bar-plot", trace, layout, config);
 
         });
     }
+   
+    
